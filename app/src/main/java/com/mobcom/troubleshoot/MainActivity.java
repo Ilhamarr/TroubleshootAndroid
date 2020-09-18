bbafba0c7fe3a8084c03375c7c3b787ca636bc1b
bagusn13 <bagusn1367@gmail.com>
2020-09-18 07:06:45 +0700

    adding Home Page

--- app/src/main/java/com/mobcom/troubleshoot/MainActivity.java
+++ app/src/main/java/com/mobcom/troubleshoot/MainActivity.java
@@ -2,7 +2,10 @@ package com.mobcom.troubleshoot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    //remove status bar backgroud (biar transparant)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+      Window w = getWindow();
+      w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+              WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
+    //end remove status bar backgroud
   }
 }
