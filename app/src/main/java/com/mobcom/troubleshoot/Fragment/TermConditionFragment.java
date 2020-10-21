fdfccba29ebb62632cbfc126c1e9a0c2c26e3bcc
Ilhamarr <ilhamarr40@gmail.com>
2020-10-21 15:55:41 +0700

    mengedit TermConditionFragment.java

--- app/src/main/java/com/mobcom/troubleshoot/Fragment/TermConditionFragment.java
+++ app/src/main/java/com/mobcom/troubleshoot/Fragment/TermConditionFragment.java
@@ -3,12 +3,15 @@ package com.mobcom.troubleshoot.Fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.mobcom.troubleshoot.R;
+import com.mobcom.troubleshoot.databinding.FragmentOrderFailedBinding;
+import com.mobcom.troubleshoot.databinding.FragmentTermConditionBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,14 +20,8 @@ import com.mobcom.troubleshoot.R;
  */
 public class TermConditionFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FragmentTermConditionBinding fragmentTermConditionBinding;
+    private NavController navController;
 
     public TermConditionFragment() {
         // Required empty public constructor
@@ -34,28 +31,28 @@ public class TermConditionFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param //param1 Parameter 1.
+     * @param //param2 Parameter 2.
      * @return A new instance of fragment TermConditionFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static TermConditionFragment newInstance(String param1, String param2) {
-        TermConditionFragment fragment = new TermConditionFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+//    public static TermConditionFragment newInstance(String param1, String param2) {
+//        TermConditionFragment fragment = new TermConditionFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
